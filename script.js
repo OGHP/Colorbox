e19f23f1b6f1e9e1ec905b3146cf64cfde453680
heatherpalmer78 <heatherpa@zillow.com>
2016-12-05 22:30:31 -0800

    Colorbox1

---
+++ script.js
@@ -0,0 +1,11 @@
+var container = document.getElementById('container')
+
+
+for (var i = 0; i < 100; i++) {
+  var elem = document.createElement('div'); 
+  //creates a new DOM element - not on page yet
+  container.appendChild(elem);
+  //insert it into the html
+
+}
+
