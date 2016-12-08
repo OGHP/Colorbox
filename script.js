7b6547b8e2a86c94da5d77eac2ad7707e1b0aeed
heatherpalmer78 <heatherpa@zillow.com>
2016-12-07 23:22:48 -0800

    Heather's Colorblock Project

--- script.js
+++ script.js
@@ -4,23 +4,62 @@ var getRandomInt = function(min,max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-for (var i = 0; i < 100; i++) {
-  var elem = document.createElement('div');  //creates DOM element not on webpage yet
+var generateBox = function(){
+var elem = document.createElement('div'); 
   elem.classList.add('box');
 
-  var rndNum = getRandomInt(1, 4);
+  var rndNum = getRandomInt(1, 5);
   console.log(rndNum);
 
   if(rndNum === 1) {
-    elem.classList.add('hotpink');
+    elem.classList.add('blue');
   } else if(rndNum === 2) {
     elem.classList.add('teal');
   } else if(rndNum === 3) {
-    elem.classList.add('yellow');
+    elem.classList.add('cream');
+  } else if(rndNum === 4) {
+    elem.classList.add('silver');
   } else {
     elem.classList.add('green');
   }
 
   container.appendChild(elem);    //add element (append) to container
 }
+
+var maxIterations = 2000;
+var iterator = 0
+
+var boxInterval = setInterval(function() {
+  if (iterator < maxIterations) {
+  generateBox();
+  iterator++;
+} else {
+  clearInterval(boxInterval);
+}
+}, 20);
+
+
+
+
+// original box code before adding interval
+// for (var i = 0; i < 100; i++) {
+//   var elem = document.createElement('div');  //creates DOM element not on webpage yet
+//   elem.classList.add('box');
+
+//   var rndNum = getRandomInt(1, 4);
+//   console.log(rndNum);
+
+//   if(rndNum === 1) {
+//     elem.classList.add('hotpink');
+//   } else if(rndNum === 2) {
+//     elem.classList.add('teal');
+//   } else if(rndNum === 3) {
+//     elem.classList.add('yellow');
+//   } else {
+//     elem.classList.add('green');
+//   }
+
+//   container.appendChild(elem);
+// }    //add element (append) to container
+
 
